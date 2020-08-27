d6f80b0d8d197ec7a991e61987ae228b8559f636
sergio santos <serginholsantos5@gmail.com>
2020-08-27 18:03:53 -0300

    subindo aplicacao

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aqui entra o inicio do codigo</h1>
+      <h1>tudo certo agora</h1>
+    </div>
+  );
+}
+
+export default App;
